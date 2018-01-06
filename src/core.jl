8c13b6d47f6fc08659b8fcde6e4450f772bec0f1
Chad Scherrer <chad.scherrer@gmail.com>
2018-01-06 07:56:45 -0800

    move observe to core.jl

--- src/core.jl
+++ src/core.jl
@@ -7,3 +7,48 @@ macro model(ex)
     body = :(function() $ex end)
     Expr(:quote, prettify(body))
 end
+
+"""
+    observe(model, var)
+"""
+function observe(model, v :: Symbol)
+    if @capture(model, function(args__) body_ end)
+        if !(v in args)
+            push!(args, v)
+        end
+    else 
+        args = [v]
+    end 
+
+    body = postwalk(body) do x 
+        if @capture(x, v0_ ~ dist_) && v0 == v
+            quote 
+                $v <~ $dist
+            end
+        else 
+            x
+        end
+    end
+
+    fQuoted = Expr(:function, :(($(args...),)), body)
+
+    return prettify(fQuoted)
+end 
+
+function observe(model, vs :: Vector{Symbol})
+    if @capture(model, function(args__) body_ end)
+        args = union(args, vs)
+    else 
+        args = vs
+    end 
+         $v <~ $dist
+            end
+        else 
+            x
+        end
+    end
+
+    fQuoted = Expr(:function, :(($(args...),)), body)
+
+    return prettify(fQuoted)
+end 
