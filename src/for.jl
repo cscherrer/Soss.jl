93c6661845ed4d782e492da7edecc496146a6290
Chad Scherrer <chad.scherrer@gmail.com>
2018-08-30 16:33:24 -0700

    make `For` a struct

--- src/for.jl
+++ src/for.jl
@@ -1,12 +1,10 @@
 using Distributions
 import Distributions.logpdf
-import Distributions.rand
-logpdf{D<:Any, T <: Any, N<:Any}(ds :: AbstractArray{D, N},xs::AbstractArray{T, N}) = sum(logpdf(dx...) for dx in zip(ds,xs))
-rand(ds :: AbstractArray{D, N} where {D,N}) = [rand(d) for d in ds]
+import Base.rand
 
-# THIS IS A HACK
-# `For` should really be a constructor. 
-# The problem with the current approach is that for a distribution d, `For(1:3) do x d end` has the type of an Array, not a Distribution.
-# But this was quick, and it works, so here it is
+struct For
+    f
+    xs
+end
 
-For = map
+rand(ex::For) = map(rand, map(ex.f,ex.xs))
