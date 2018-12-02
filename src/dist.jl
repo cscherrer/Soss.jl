556c466ded35f7752a8138d869e8c8788ac33b1a
Chad Scherrer <chad.scherrer@gmail.com>
2018-12-02 07:48:39 -0800

    HalfCauchy stuff

--- src/dist.jl
+++ src/dist.jl
@@ -1,11 +1,7 @@
 export LogisticBinomial, HalfCauchy
 
 HalfCauchy(s) = Truncated(Cauchy(0,s),0,Inf)
-
-import Distributions.support
-export support
-support(::typeof(HalfCauchy)) = RealInterval(0, Inf)
-
+HalfCauchy() = Truncated(Cauchy(0,1.0),0,Inf)
 
 # Binomial distribution, parameterized by logit(p)
 LogisticBinomial(n,x)=Binomial(n,logistic(x))
