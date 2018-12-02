d3c77ee23eeca184f32a06b28554b4cee364c8a9
Chad Scherrer <chad.scherrer@gmail.com>
2018-12-02 07:47:55 -0800

    Changing around iid

--- src/iid.jl
+++ src/iid.jl
@@ -1,16 +1,16 @@
 using Distributions
+import Distributions.logpdf
 import Base.rand
 export iid
 
 struct iid
-    n :: Int
-    dist 
+    # n :: Int
+    dist
 end
 
-iid(n) = dist -> iid(n,dist)
+# iid(n) = dist -> iid(n,dist)
+
 
 rand(ndist::iid) = rand(ndist.dist, ndist.n)
 
-
-
-
+logpdf(d::iid,x) = sum(logpdf.(d.dist,x))
