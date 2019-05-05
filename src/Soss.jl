d0721abab9c91b7002e8d671bd26da698893e17c
Chad Scherrer <chad.scherrer@gmail.com>
2019-05-05 08:10:45 -0700

    Comment out most includes (for now)

--- src/Soss.jl
+++ src/Soss.jl
@@ -9,20 +9,22 @@ using Reexport: @reexport
 import MacroTools: prewalk, postwalk, @q, striplines, replace, flatten, @capture
 import MLStyle
 
+# include("ast.jl")
 include("model.jl")
-include("weighted.jl")
-include("rand.jl")
-include("dist.jl")
-include("utils.jl")
-include("iid.jl")
-include("for.jl")
-include("flat.jl")
+# include("weighted.jl")
+# include("rand.jl")
+# include("dist.jl")
+# include("utils.jl")
+# include("iid.jl")
+# include("for.jl")
+# include("flat.jl")
 include("examples.jl")
-include("bijections.jl")
-include("graph.jl")
-include("nuts.jl")
-include("optim.jl")
-include("importance.jl")
+# include("bijections.jl")
+# include("graph.jl")
+# include("nuts.jl")
+# include("optim.jl")
+# include("importance.jl")
+# include("canonical.jl")
 
 
 end # module
