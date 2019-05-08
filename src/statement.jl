0019168b72594293ed4fe5ed0871fc10b090067b
Chad Scherrer <chad.scherrer@gmail.com>
2019-05-08 11:28:33 -0700

    Statement type and associated functions

--- src/statement.jl
+++ src/statement.jl
@@ -2,19 +2,37 @@ using MLStyle
 
 abstract type Statement end
 
+Statement(x) = convert(Statement, x)
+
 struct Let <: Statement
     name :: Symbol
     value
 end
+
+varName(st :: Let) = st.name
 
 struct Follows <: Statement
     name :: Symbol
     dist 
 end
 
-function convert(Statement, expr::Expr)
+varName(st :: Follows) = st.name
+
+struct Return <: Statement
+    value 
+end
+
+varName(st :: Return) = nothing
+
+function convert(Statement, expr :: Expr)
     @match expr begin
         :($x ~ $dist)  => Follows(x, dist)
         :($x = $value) => Let(x, value)
+        :(return $x)   => Return(x)
     end
 end
+
+varName(::Nothing) = nothing
+
+convert(Statement, node :: LineNumberNode) = nothing
+
