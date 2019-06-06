8fc0566b6e6f4a97d85028e3dc87c51bb43c36dc
Chad Scherrer <chad.scherrer@gmail.com>
2019-06-06 07:05:44 -0700

    Model constructors

--- src/model.jl
+++ src/model.jl
@@ -4,35 +4,20 @@ using MLStyle
 
 abstract type AbstractModel end
 
-struct Model{T} <: AbstractModel
+struct Model <: AbstractModel
     args :: Vector{Symbol}
-    vars :: Vector{Symbol}
     body :: Vector{Statement}
-    meta :: Dict{Symbol, Any}
 end
-
 
 function Model(vs :: Vector{Symbol}, expr :: Expr)
-    body = filter([Statement(x) for x in expr.args]) do x
-        !isnothing(x)
-    end
-    @show body
-    vars = union(vs, [varName(s) for s in body]...)
-    Model(Vector{Symbol}(vs.args), body)
+    body = [Statement(x) for x in expr.args]
+    Model(vs, body)
 end
-
-# function Model(vs::Vector{Symbol}, body::Vector{Statement})
-
-#     m = justArgs(vs)
-#     # Add all the lines!
-#     foldl(merge, body.args; init=justArgs(vs))
-# end
 
 macro model(vs::Expr,expr::Expr)
     @assert vs.head == :tuple
     @assert expr.head == :block
     Model(Vector{Symbol}(vs.args), expr)
-    # Model(Vector{Symbol}(vs.args), pretty(body)) |> expandSubmodels
 end
 
 macro model(v::Symbol, expr::Expr)
@@ -46,30 +31,9 @@ end
 
 (m::Model)(vs...) = begin
     args = m.args ∪ vs
-    vars = m.vars ∪ vs
-    
-
-    Model(args, m.vars, m.stoch, m.bound, m.retn) |> condition(args...)
+    Model(args, m.body) |> condition(args...)
 end
 
-# # (m::Model)(;kwargs...) = begin
-# #     result = deepcopy(m)
-# #     args = result.args
-# #     body = result.body
-# #     vs = keys(kwargs)
-# #     setdiff!(args, vs)
-# #     assignments = [:($k = $v) for (k,v) in kwargs]
-# #     pushfirst!(body.args, assignments...)
-# #     stoch = stochastic(m)
-# #     Model(args, body) |> condition(vs...) |> flatten
-# # end
-
-# # inline for now
-# # TODO: Be more careful about this
-# (m::Model)(;kwargs...) = begin
-#     m = condition(keys(kwargs)...)(m)
-#     kwargs = Dict(kwargs)
-#     leaf(v) = get(kwargs, v, v)
 
 function Base.show(io::IO, m::Model) 
     print(io, "@model ")
@@ -102,11 +66,11 @@ function Base.show(io :: IO, st :: Let)
     indent = get(io, :indent, 0)
     print(repeat("    ",indent))
     println(io, st.name, " = ", st.value)
-    end
+end
 
 function Base.show(io :: IO, st :: LineNumber)
     return ()
-    end
+end
 
 function Base.show(io, st :: Return)
     indent = get(io, :indent, 0)
