b214da367a875611f2c339faa6222a516be1cde9
Chad Scherrer <chad.scherrer@gmail.com>
2019-06-07 09:38:57 -0700

    Reworking how to show a model

--- src/model.jl
+++ src/model.jl
@@ -28,56 +28,33 @@ macro model(expr :: Expr)
     Model(Vector{Symbol}(), expr) 
 end
 
-
 (m::Model)(vs...) = begin
     args = m.args ∪ vs
     Model(args, m.body) |> condition(args...)
 end
 
-
 function Base.show(io::IO, m::Model) 
-    print(io, "@model ")
-    indent = get(io, :indent, 0)
+    print(io, convert(Expr, m))
+end
+
+convert(::Type{Expr}, st::Follows) = :($(st.name) ~ $(st.value))
+convert(::Type{Expr}, st::Let) = :($(st.name) = $(st.value))
+convert(::Type{Expr}, st::LineNumber) = st.node
+convert(::Type{Expr}, st::Return) = :(return $(st.value))
+
+function convert(::Type{Expr}, sts::Vector{Statement})
+    Expr(:block, [convert(Expr, st) for st in sts]...)
+end
+
+function convert(::Type{Expr}, m::Model)
     numArgs = length(m.args)
-    
-    if numArgs == 1
-        print(m.args[1], " ")
+    args = if numArgs == 1
+       m.args[1]
     elseif numArgs > 1
-        print(io, "$(Expr(:tuple, [x for x in m.args]...)) ")
+        Expr(:tuple, [x for x in m.args]...)
     end
-    println(io, "begin")
-
-    io2 = IOContext(io, :indent => indent + 1)
-    for st in m.body
-        print(io2, st)
+    q = @q begin
+        @model $(args) $(convert(Expr, m.body))
     end
-    print("end")
-
+    striplines(q).args[1]
 end
-
-function Base.show(io :: IO, st :: Follows)
-    indent = get(io, :indent, 0)
-    print(repeat("    ",indent))
-    print(io, st.name, " ~ ")
-    printdist(io, st.value)
-end
-
-function Base.show(io :: IO, st :: Let)
-    indent = get(io, :indent, 0)
-    print(repeat("    ",indent))
-    println(io, st.name, " = ", st.value)
-end
-
-function Base.show(io :: IO, st :: LineNumber)
-    return ()
-end
-
-function Base.show(io, st :: Return)
-    indent = get(io, :indent, 0)
-    print(repeat("    ",indent))
-    println("return ",value)
-end
-
-
-# function printdist(io, dist)
-#     @match dist begin
