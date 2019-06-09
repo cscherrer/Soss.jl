1533de747367a680df3ac1688959230cf0c9c8a5
Chad Scherrer <chad.scherrer@gmail.com>
2019-06-09 09:10:58 -0700

    rework sourceRand

--- src/rand.jl
+++ src/rand.jl
@@ -3,36 +3,6 @@ export sourceRand
 export argtuple
 argtuple(m) = Expr(:tuple,arguments(m)...)
 
-function sourceRand(m :: Model)
-    body = postwalk(m.body) do x
-        if @capture(x, v_ ~ dist_)
-            qv = QuoteNode(v)
-            @q ($v = rand($dist))
-        else x
-        end
-    end
-
-    @gensym rand
-
-    argsExpr = Expr(:tuple,arguments(m)...)
-
-    # Pack stochastic variables into a NamedTuple
-    stochExpr = begin
-        vals = map(stochastic(m)) do x Expr(:(=), x,x) end
-        Expr(:tuple, vals...)
-    end
-    #Wrap in a function to avoid global variables
-    flatten(@q (
-        function $rand(args...;kwargs...) 
-            @unpack $argsExpr = kwargs
-            # kwargs = Dict(kwargs)
-            $body
-            $stochExpr
-        end
-    ))
-end
-
-
 
 export logWeightedRand
 function logWeightedRand(m :: Model, N :: Int)
@@ -72,3 +42,12 @@ end
 
 export rand
 rand(m::Model; kwargs...) = makeRand(m)(;kwargs...)
+
+function sourceRand(m::Model)
+    buildExpr!(ctx, st::Let)     = :($(st.name) = $(st.value))
+    buildExpr!(ctx, st::Follows) = :($(st.name) = rand($(st.value)))
+    buildExpr!(ctx, st::Return)  = :(return $(st.value))
+    buildExpr!(ctx, st::LineNumber) = nothing
+
+    buildSource(m, buildExpr!)
+end
