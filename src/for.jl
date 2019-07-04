a066ccaa65f8e93d2fb62854e2f6353387029b1a
Chad Scherrer <chad.scherrer@gmail.com>
2019-07-04 06:41:19 -0700

    Rearrange parameters in For

--- src/for.jl
+++ src/for.jl
@@ -2,10 +2,13 @@ using Distributions
 import Distributions.logpdf
 using Parameters
 
+# D is type type of the base distribution
+# T is the type of Parameters
+# X is the type of observations
 export For
-struct For{D,T,X}
-    f # returns a distribution of type D
-    θs :: Array{T}
+struct For{T,X,D} # <: Distribution{Multivariate,S} where {T, X, D <: Distribution{V,X} where V <: VariateForm, S <: ValueSupport} # where {A, D <: Distribution{V,A} where V, T, X} 
+    f   # f(θ) returns a distribution of type D
+    θs :: AbstractArray{T}
 end
 
 function For(f, θs) 
@@ -13,18 +16,18 @@ function For(f, θs)
     d = f(θs[1])
     D = typeof(d)
     X = eltype(d)
-    For{D,T,X}(f,θs)
+    For{T,X,D}(f,θs)
 end
 
 export rand
 
-Base.rand(dist::For{D,T,X} where {D,T,X}) = map(rand, map(dist.f,dist.θs))
+Base.rand(dist::For{T,X,D} where {T,X,D}) = map(rand, map(dist.f,dist.θs))
 
 # Distributions.logpdf(dist::For, xs) = logpdf.(map(dist.f, dist.θs), xs) |> sum
 
 
 
-@inline function Distributions.logpdf(d::For{D,T,X},x::Array{X}) where {D,T,X}
+@inline function Distributions.logpdf(d::For{T,X,D},x::AbstractArray{X}) where {T,X,D}
     @unpack (f,θs) = d
 
     s = 0.0
