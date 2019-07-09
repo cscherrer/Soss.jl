bbdcad8a272a1dd0e19201170248fc60ea17ead9
Chad Scherrer <chad.scherrer@gmail.com>
2019-07-08 20:15:10 -0700

    clean up rand

--- src/rand.jl
+++ src/rand.jl
@@ -3,37 +3,9 @@
 export sourceRand
 
 export argtuple
-argtuple(m) = Expr(:tuple,arguments(m)...)
+argtuple(m) = arguments(m) |> astuple
 
-
-export logWeightedRand
-function logWeightedRand(m :: Model, N :: Int)
-
-    body = postwalk(m.body) do x
-        if @capture(x, v_ ~ dist_)
-            @q begin
-                $v = rand($dist)
-                val = merge(val, ($v=$v,))
-            end
-        else x
-        end
-    end
-
-    #Wrap in a function to avoid global variables
-    result = @q () -> begin
-        ans = []
-        for n in 1:$N
-            ℓ = 0.0
-            val = NamedTuple()
-            $body
-            push!(ans,Weighted(val,ℓ))
-        end
-        ans
-    end
-
-    return Base.invokelatest(eval(result))
-
-end
+astuple(x) = Expr(:tuple,x...)
 
 
 export makeRand
@@ -72,7 +44,6 @@ function sourceRand(m::Model)
     flatten(@q (
         function $rand(args...;kwargs...) 
             @unpack $argsExpr = kwargs
-            # kwargs = Dict(kwargs)
             $body
             $stochExpr
         end
