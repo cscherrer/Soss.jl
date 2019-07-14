7251e143017e5355bd8ebe7eaaad3a7c6d70151d
Chad Scherrer <chad.scherrer@gmail.com>
2019-07-13 20:31:19 -0700

    qualify "transform"

--- src/nuts.jl
+++ src/nuts.jl
@@ -26,6 +26,6 @@ function nuts(m :: Model; kwargs...)
     P = TransformedLogDensity(t, f)
     ∇P = ADgradient(:ForwardDiff, P)
     chain, tuning = NUTS_init_tune_mcmc(∇P, 1000);
-    samples = transform.(Ref(parent(∇P).transformation), get_position.(chain));
+    samples = TransformVariables.transform.(Ref(parent(∇P).transformation), get_position.(chain));
     NUTS_result(chain, t, samples, tuning)
 end
