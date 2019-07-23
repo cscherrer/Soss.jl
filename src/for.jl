962912e7c368fe4bd10fc36f30953f2f60de4ed1
Chad Scherrer <chad.scherrer@gmail.com>
2019-07-23 16:39:21 -0400

    Beautiful "For" type params were breaking things :(

--- src/for.jl
+++ src/for.jl
@@ -6,34 +6,34 @@ using Parameters
 # T is the type of Parameters
 # X is the type of observations
 export For
-struct For{T,X,D} # <: Distribution{Multivariate,S} where {T, X, D <: Distribution{V,X} where V <: VariateForm, S <: ValueSupport} # where {A, D <: Distribution{V,A} where V, T, X} 
+struct For # <: Distribution{Multivariate,S} where {T, X, D <: Distribution{V,X} where V <: VariateForm, S <: ValueSupport} # where {A, D <: Distribution{V,A} where V, T, X} 
     f   # f(θ) returns a distribution of type D
-    θs :: AbstractArray{T}
+    θs :: AbstractArray
 end
 
-function For(f, θs) 
-    T = eltype(θs)
-    d = f(θs[1])
-    D = typeof(d)
-    X = eltype(d)
-    For{T,X,D}(f,θs)
-end
+# function For(f, θs) 
+#     T = eltype(θs)
+#     d = f(θs[1])
+#     D = typeof(d)
+#     X = eltype(d)
+#     For(f,θs)
+# end
 
 export rand
 
-Base.rand(dist::For{T,X,D} where {T,X,D}) = map(rand, map(dist.f,dist.θs))
+Base.rand(dist::For) = map(rand, map(dist.f,dist.θs))
 
 # Distributions.logpdf(dist::For, xs) = logpdf.(map(dist.f, dist.θs), xs) |> sum
 
 
 
-@inline function Distributions.logpdf(d::For{T,X,D},x::AbstractArray{X}) where {T,X,D}
+@inline function Distributions.logpdf(d::For,x::AbstractArray)
     @unpack (f,θs) = d
 
     s = 0.0
     @inbounds @simd for j in eachindex(x)
-        θ = θs[j]::T 
-        s += logpdf(f(θ)::D, x[j]::X)
+        θ = θs[j]
+        s += logpdf(f(θ), x[j])
     end
     s
 end
