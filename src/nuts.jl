39afb2f28c5a82035e0a9e3f1c8f5ab8a8f1a06e
Chad Scherrer <chad.scherrer@gmail.com>
2019-08-03 18:22:01 -0700

    invokefrozen

--- src/nuts.jl
+++ src/nuts.jl
@@ -13,42 +13,20 @@ Base.show(io::IO, n::NUTS_result) = begin
     println(IOContext(io, :limit => true, :compact => true), n.samples)
 end
 
+
 export nuts
 
-@inline function invokefrozen(f, rt, args...; kwargs...)
-    if isempty(kwargs)
-        return _invokefrozen(f, rt, args)
-    end
-    # We use a closure (`inner`) to handle kwargs.
-    inner() = f(rt, args...; kwargs...)
-    _invokefrozen(inner)
-end
-
-@inline @generated function _invokefrozen(f, ::Type{rt}, args...) where rt
-  tupargs = Expr(:tuple,(a==Nothing ? Int : a for a in args)...)
-  quote
-    _f = $(Expr(:cfunction, Base.CFunction, :f, rt, :((Core.svec)($((a==Nothing ? Int : a for a in args)...))), :(:ccall)))
-    return ccall(_f.ptr,rt,$tupargs,$((:(getindex(args,$i) === nothing ? 0 : getindex(args,$i)) for i in 1:length(args))...))
-  end
-end
-
-# TODO: possibly make this an intrinsic
-inferencebarrier(@nospecialize(x)) = Ref{Any}(x)[]
-
-
-
 function nuts(m :: Model; kwargs...)
+    f1 = makeLogdensity(m)
+    ℓ(pars) = f1(merge((;kwargs...),pars))
 
-    sourceLogdensity(m) |> eval
-
-    invokefrozen(logdensity, Float64,merge(kwargs, pairs(pars)))
     result = NUTS_result{}
     t = xform(m; kwargs...)
-    
-    f(pars) = logdensity(merge(kwargs, pairs(pars)))
-    P = TransformedLogDensity(t, f)
+    P = TransformedLogDensity(t, ℓ)
     ∇P = ADgradient(:ForwardDiff, P)
     chain, tuning = NUTS_init_tune_mcmc(∇P, 1000);
     samples = TransformVariables.transform.(Ref(parent(∇P).transformation), get_position.(chain));
     NUTS_result(chain, t, samples, tuning)
 end
+
+
