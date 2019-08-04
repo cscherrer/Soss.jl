84c6af867a18e2ab00770b06c5adf0fc7972b4de
Chad Scherrer <chad.scherrer@gmail.com>
2019-08-04 12:46:13 -0700

    reworking NUTS

--- src/nuts.jl
+++ src/nuts.jl
@@ -17,11 +17,25 @@ end
 export nuts
 
 function nuts(m :: Model; kwargs...)
-    f1 = makeLogdensity(m)
-    ℓ(pars) = f1(merge((;kwargs...),pars))
+    ℓ = makeLogdensity(m)
 
     result = NUTS_result{}
-    t = xform(m; kwargs...)
+    t = xform(m; data...)
+    P = TransformedLogDensity(t, ℓ)
+    ∇P = ADgradient(:ForwardDiff, P)
+    chain, tuning = NUTS_init_tune_mcmc(∇P, 1000);
+    samples = TransformVariables.transform.(Ref(parent(∇P).transformation), get_position.(chain));
+    NUTS_result(chain, t, samples, tuning)
+end
+
+
+
+function nuts(m :: Model, data :: NamedTuple; kwargs...)
+    f1 = makeLogdensity(m)
+    ℓ(pars) = f1(merge(data,pars))
+
+    result = NUTS_result{}
+    t = xform(m; data...)
     P = TransformedLogDensity(t, ℓ)
     ∇P = ADgradient(:ForwardDiff, P)
     chain, tuning = NUTS_init_tune_mcmc(∇P, 1000);
