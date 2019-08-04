84ac9852c8b8314c6084efe01aa5c688f54e8d68
Chad Scherrer <chad.scherrer@gmail.com>
2019-08-04 14:38:27 -0700

    advancedhmc

--- src/advancedhmc.jl
+++ src/advancedhmc.jl
@@ -3,33 +3,57 @@ using Distributions: logpdf, MvNormal
 using DiffResults: GradientResult, value, gradient
 using ForwardDiff: gradient!
 
-const D = 10
-const target = MvNormal(zeros(D), ones(D))
-ℓπ(θ) = logpdf(target, θ)
-
-function ∂ℓπ∂θ(θ)
-    res = GradientResult(θ)
-    gradient!(res, ℓπ, θ)
-    return (value(res), gradient(res))
-end
 
 ### Build up a HMC sampler to draw samples
 using AdvancedHMC
 
-# Sampling parameter settings
-n_samples = 100_000
-n_adapts = 2_000
 
-# Draw a random starting points
-θ_init = randn(D)
 
-# Define metric space, Hamiltonian, sampling method and adaptor
-metric = DiagEuclideanMetric(D)
-h = Hamiltonian(metric, ℓπ, ∂ℓπ∂θ)
-prop = NUTS(Leapfrog(find_good_eps(h, θ_init)))
-adaptor = StanHMCAdaptor(n_adapts, Preconditioner(metric), NesterovDualAveraging(0.8, prop.integrator.ϵ))
+export advhmc
 
-# Draw samples via simulating Hamiltonian dynamics
-# - `samples` will store the samples
-# - `stats` will store statistics for each sample
-samples, stats = sample(h, prop, θ_init, n_samples, adaptor, n_adapts; progress=true)
+
+function advhmc(m :: Model; kwargs...)
+    advhmc(m, NamedTuple(); kwargs...)
+end
+
+function advhmc(m :: Model, data :: NamedTuple; kwargs...)
+    logdens = makeLogdensity(m)
+    ℓ(pars) = logdens(merge(data,pars))
+
+    t = xform(m; data...)
+
+
+    function f(x) 
+        (θ, logjac) = transform_and_logjac(t,x)
+        ℓ(θ) + logjac
+    end
+
+    
+    function ∂f(x)
+        res = GradientResult(x)
+        gradient!(res, f, x)
+        return (value(res), gradient(res))
+    end
+
+    # Sampling parameter settings
+    n_samples = 1000
+    n_adapts = 1000
+
+    D = t.dimension
+    # Draw a random starting points
+    x_init = randn(D)
+
+    # Define metric space, Hamiltonian, sampling method and adaptor
+    metric = DiagEuclideanMetric(D)
+    h = Hamiltonian(metric, f, ∂f)
+    prop = AdvancedHMC.NUTS(Leapfrog(find_good_eps(h, x_init)))
+    adaptor = StanHMCAdaptor(n_adapts, Preconditioner(metric), NesterovDualAveraging(0.8, prop.integrator.ϵ))
+
+    # Draw samples via simulating Hamiltonian dynamics
+    # - `samples` will store the samples
+    # - `stats` will store statistics for each sample
+    samples, stats = sample(h, prop, x_init, n_samples, adaptor, n_adapts; progress=true)
+
+end
+
+
