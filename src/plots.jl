6c349ae3ccce8a29738de967faf66c2a18cd997b
Chad Scherrer <chad.scherrer@gmail.com>
2019-08-06 20:46:13 -0700

    plots

---
+++ src/plots.jl
@@ -0,0 +1,12 @@
+using Plots
+
+@userplot EachPlot
+
+@recipe function plt(p::EachPlot)
+    x,y = p.args
+    label --> ""
+    color --> :black
+    alpha --> 0.01
+    m = Matrix(y)'
+    x,m
+end
