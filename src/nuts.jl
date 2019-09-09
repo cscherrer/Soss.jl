d70949f2b1baceb444107d01d21e2fa17d731931
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-08 19:12:21 -0700

    DynamicHMC.NoProgressReport

--- src/nuts.jl
+++ src/nuts.jl
@@ -13,7 +13,7 @@ function nuts(m :: BoundModel{A,B}, _data) where {A,B}
     t = xform(m,_data)
     P = TransformedLogDensity(t, ℓ)
     ∇P = ADgradient(Val(:Flux), P)
-    results = mcmc_with_warmup(MersenneTwister(), ∇P, 1000);
+    results = mcmc_with_warmup(MersenneTwister(), ∇P, 1000; reporter=DynamicHMC.NoProgressReport());
     samples = TransformVariables.transform.(parent(∇P).transformation, results.chain)
     # ... here you may want to return: samples, results.chain, results.tree_statistics,
     #     results.κ, results.ϵ in your container type
