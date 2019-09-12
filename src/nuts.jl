4d2a88f3506a5a62125077ac759663d48ce4de81
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-12 10:20:24 -0700

    rm Flux dependency for now (weird CUDAdrv error)

--- src/nuts.jl
+++ src/nuts.jl
@@ -5,14 +5,14 @@ using Random
 
 export nuts
 
-import Flux
+# import Flux
 function nuts(m :: BoundModel{A,B}, _data) where {A,B}
     rng = MersenneTwister()
     ℓ(pars) = logpdf(m, merge(pars, _data))
 
     t = xform(m,_data)
     P = TransformedLogDensity(t, ℓ)
-    ∇P = ADgradient(Val(:Flux), P)
+    ∇P = ADgradient(Val(:ForwardDiff), P)
     results = mcmc_with_warmup(MersenneTwister(), ∇P, 1000; reporter=DynamicHMC.NoProgressReport());
     samples = TransformVariables.transform.(parent(∇P).transformation, results.chain)
     # ... here you may want to return: samples, results.chain, results.tree_statistics,
