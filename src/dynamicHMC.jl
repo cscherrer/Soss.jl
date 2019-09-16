efca6fd41fd5c8249855f25d1a17547a424b382f
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-15 19:37:39 -0700

    streaming dynamicHMC

--- src/dynamicHMC.jl
+++ src/dynamicHMC.jl
@@ -6,14 +6,43 @@ using Random
 export dynamicHMC
 
 # import Flux
+function dynamicHMC(m :: JointDistribution, _data, N::Int) 
+    ℓ(pars) = logpdf(m, merge(pars, _data))
+
+    t = xform(m,_data)
+    P = TransformedLogDensity(t, ℓ)
+    ∇P = ADgradient(Val(:ForwardDiff), P)
+    results = mcmc_with_warmup(MersenneTwister(), ∇P, N; reporter=DynamicHMC.NoProgressReport());
+    samples = TransformVariables.transform.(parent(∇P).transformation, results.chain)
+end
+
+
 function dynamicHMC(m :: JointDistribution, _data) 
     ℓ(pars) = logpdf(m, merge(pars, _data))
 
     t = xform(m,_data)
     P = TransformedLogDensity(t, ℓ)
     ∇P = ADgradient(Val(:ForwardDiff), P)
-    results = mcmc_with_warmup(MersenneTwister(), ∇P, 1000; reporter=DynamicHMC.NoProgressReport());
-    samples = TransformVariables.transform.(parent(∇P).transformation, results.chain)
-    # ... here you may want to return: samples, results.chain, results.tree_statistics,
-    #     results.κ, results.ϵ in your container type
+    
+    # initialization
+    rng = MersenneTwister()
+    results = DynamicHMC.mcmc_keep_warmup(rng, ∇P, 0; reporter = NoProgressReport())
+    steps = DynamicHMC.mcmc_steps(results.sampling_logdensity, results.final_warmup_state)
+
+    (results, steps)
 end
+
+
+using ResumableFunctions
+
+export stream
+@resumable function stream(m :: JointDistribution, _data::NamedTuple) 
+    t = xform(m, _data)
+    (results, steps) = dynamicHMC(m, _data)
+    Q = results.final_warmup_state.Q
+    while true
+        Q, tree_stats = DynamicHMC.mcmc_next_step(steps, Q)
+        @yield (merge(t(Q.q), (_ℓ = Q.ℓq,)), tree_stats)
+    end
+end
+
