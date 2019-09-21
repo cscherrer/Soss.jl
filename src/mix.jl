f4a25e1743c971e420f0961e27b9d889e4ea0853
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-21 07:16:51 -0700

    Bugfixes for `Mix`

--- src/mix.jl
+++ src/mix.jl
@@ -7,16 +7,27 @@ end
 export Mix
 Mix(w::Vector) = dists -> Mix(dists, log.(w))
 
-function rand(mix::Mix)
+function Base.rand(mix::Mix)
     # This is the "Gumbel max trick"  for categorical sampling
     j = argmax(mix.logweights .+ rand(Gumbel()))
     mix.dists.f(mix.dists.θs[j]) |> rand
 end
 
-function logpdf(mix::Mix, x)
+function Base.rand(mix::Mix, N::Int)
+    x1 = rand(mix)
+    x = Vector{typeof(x1)}(undef, N)
+    @inbounds x[1] = x1
+    @inbounds for n in 2:N
+        x[n] = rand(mix)
+    end
+    x
+end
+
+
+function Distributions.logpdf(mix::Mix, x)
     ℓ = 0.0
     for (j, θ) in enumerate(mix.dists.θs)
-        ℓ = logsumexp(ℓ, mix.logweights[j] + logpdf(mix.dists.f(θ), x))
+        ℓ = logaddexp(ℓ, mix.logweights[j] + logpdf(mix.dists.f(θ), x))
     end
     ℓ
 end
