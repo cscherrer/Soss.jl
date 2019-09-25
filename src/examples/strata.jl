7b4f5065a5c80408bab880499420d5e521509a68
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-25 09:30:16 -0700

    Code for strata

--- src/examples/strata.jl
+++ src/examples/strata.jl
@@ -3,11 +3,13 @@ using Soss
 
 
 mt = @model x begin
-    α ~ Cauchy()
-    β ~ Normal()
-    σ ~ HalfNormal()
-    y ~ For(x) do xj
-        Mix([Normal(α + β * xj, σ), StudentT(5, α + β * xj, 5*σ)], [0.9,0.1])
+    α = 1.0
+    β = 3.0
+    σ = 0.5
+    yhat = α .+ β .* x
+    y ~ For(eachindex(x)) do j
+        # StudentT(3,yhat[j], σ)
+        Mix([Normal(yhat[j], σ), Normal(yhat[j],10σ)], [0.8,0.2])
     end
 end;
 
@@ -28,6 +30,7 @@ truth = let
     (α = d["α"], β = d["β"], σ = d["σ"], x = d["x"], y = d["y"])
 end
 
+x = truth.x
 
 using Plots
 xx = range(extrema(truth.x)...,length=100)
@@ -38,33 +41,99 @@ savefig("rawdata.png")
 m = @model x begin
     α ~ Cauchy()
     β ~ Normal()
-    σ ~ HalfNormal()
+    σ ~ HalfNormal(10)
     yhat = α .+ β .* x
     y ~ For(eachindex(x)) do j
-        Normal(yhat[j], σ)
-    end
+            Normal(yhat[j], σ)
+        end
 end;
 
 
 
-post = dynamicHMC(m(x=truth.x), (y=truth.y,)) #|> particles
+post = dynamicHMC(m(x=truth.x), (y=truth.y,)) 
 
-eachplot(xx, post.α .+ post.β .* xx, lw=3, dpi=300)
+ppost = particles(post)
+
+eachplot(xx, ppost.α .+ ppost.β .* xx, lw=3, dpi=300)
 scatter!(truth.x,truth.y, legend=false, c=1)
 plot!(xx, truth.α .+ truth.β .* xx, legend=false, lw=3, c=2)
-ylims!(-3,5)
 savefig("fit1.png")
 
 pred = predictive(m, :α, :β, :σ)
 
-postpred= [rand(pred(θ)((x=x,))) for θ ∈ post] |> particles
-p = sortperm(x)
-eachplot(x[p], (truth.y - postpred.y)[p])
+postpred = [rand(pred(θ)((x=x,))) for θ ∈ post] |> particles
+
+pvals = mean.(truth.y .> postpred.y)
+
+# PPC vs x
+scatter(truth.x, pvals, legend=false, dpi=300)
+xlabel!("x")
+ylabel!("Bayesian p-value")
+savefig("ppc-x.png")
 
 
-eachplot(x, ppc.yhat, lw=3)
-scatter!(truth.x,truth.y, legend=false)
+# PPC vs y
+scatter(truth.y, pvals, legend=false, dpi=300)
+xlabel!("y")
+ylabel!("Bayesian p-value")
+savefig("ppc-y.png")
+
+using AverageShiftedHistograms
+
+o = ash(pvals, rng=0:0.01:1, kernel=Kernels.cosine,m=8)
+plot(o, legend=false,dpi=300)
+xlabel!("Bayesian p-values")
+savefig("ppc.png")
+
+
+##########################################
+
+m2 = @model x begin
+    α ~ Cauchy()
+    β ~ Normal()
+    σ ~ HalfNormal(10)
+    νinv ~ HalfNormal()
+    yhat = α .+ β .* x
+    y ~ For(eachindex(x)) do j
+            StudentT(1/νinv,yhat[j],σ)
+        end
+end;
+
+post2 = dynamicHMC(m2(x=truth.x), (y=truth.y,))
+ppost2 = particles(post2)
+
+
+
+eachplot(xx, ppost2.α .+ ppost2.β .* xx, lw=3, dpi=300)
+scatter!(truth.x,truth.y, legend=false, c=1)
 plot!(xx, truth.α .+ truth.β .* xx, legend=false, lw=3, c=2)
-ylims!(-3,5)
+savefig("fit2.png")
 
-eachplot(xx, )
+pred2 = predictive(m2, setdiff(stochastic(m2), [:y])...)
+
+post2pred= [rand(pred2(θ)((x=x,))) for θ ∈ post2]  |> particles
+
+pvals2 = mean.(truth.y .> post2pred.y)
+
+# PPC vs x
+scatter(truth.x, pvals2, legend=false, dpi=300)
+xlabel!("x")
+ylabel!("Bayesian p-value")
+savefig("ppc2-x.png")
+
+
+# PPC vs y
+scatter(truth.y, pvals2, legend=false, dpi=300)
+xlabel!("y")
+ylabel!("Bayesian p-value")
+savefig("ppc2-y.png")
+
+o = ash(pvals2, rng=0:0.01:1, kernel=Kernels.cosine,m=8)
+plot(o, legend=false,dpi=300)
+xlabel!("Bayesian p-values")
+savefig("ppc2.png")
+
+
+
+
+
