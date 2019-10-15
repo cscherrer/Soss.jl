de6f663fc800435093baf3e6b1e0d97226a737a1
Chad Scherrer <chad.scherrer@gmail.com>
2019-10-15 06:24:35 -0700

    More type shenanigans in For

--- src/for.jl
+++ src/for.jl
@@ -3,19 +3,26 @@ import Distributions.logpdf
 using Parameters
 
 export For
-struct For{F,N,T} 
+struct For{F,N,T,X} 
     f :: F  
     θ :: NTuple{N,T}
 end
 
 
-function For(f::F, θ::T...) where {F,T <: AbstractRange}
-    For{F,length(θ),  T}(f,θ)
+function For(f::F, θ::T...) where {F,T}
+    N = length(θ)
+    X = f.(ones(N)...) |> eltype
+    For{F,N,T,X}(f,θ)
+end
+
+function For(f::F, θ::NTuple{N,T}) where {N,F,T}
+    X = f.(ones(N)...) |> eltype
+    For{F,N,T,X}(f,θ)
 end
 
 export rand
 
-function Base.rand(dist::For{F,N,T}) where {F,N,T <: AbstractRange}
+function Base.rand(dist::For) 
     map(CartesianIndices(dist.θ)) do I
         (rand ∘ dist.f)(Tuple(I)...)
     end
@@ -27,12 +34,24 @@ using Base.Cartesian
 export logpdf
 
 
-@inline function logpdf(d::For{F,N,T},xs::AbstractArray{X,N}) where {F,N,T <: AbstractRange, X}
+@inline function logpdf(d::For{F,N,T},xs::AbstractArray{X,N}) where {F,N,T, X}
     s = 0.0
     @inbounds @simd for θ in CartesianIndices(d.θ)
         s += logpdf(d.f(Tuple(θ)...), xs[θ])
     end
     s
+end
+
+@inline function importanceSample(p::For{F1,N,T,X}, q::For{F2,N,T,X}) where {F1,F2,N,T,X}
+    _ℓ = 0.0
+    x = Array{X, N}(undef, length.(q.θ))
+    @inbounds @simd for θ in CartesianIndices(q.θ)
+        I = Tuple(θ)
+        ℓx = importanceSample(p.f(I...), q.f(I...))
+        _ℓ += ℓx.ℓ
+        x[θ] = ℓx.val
+    end
+    Weighted(_ℓ,x)
 end
 
 using Transducers
