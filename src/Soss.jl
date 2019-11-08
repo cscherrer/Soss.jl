67102a27bcf7d4f070219338427d9fd71808aee4
Chad Scherrer <chad.scherrer@gmail.com>
2019-11-08 10:36:44 -0800

    Reorganizing (Good idea Kusti)

--- src/Soss.jl
+++ src/Soss.jl
@@ -15,39 +15,47 @@ using LinearAlgebra
 using LazyArrays
 using FillArrays
 
-include("statement.jl")
-include("model.jl")
-# include("weighted.jl")
+include("core/statement.jl")
+include("core/model.jl")
+include("core/canonical.jl")
+include("core/dependencies.jl")
+include("core/toposort.jl")
+include("core/weighted.jl")
+include("core/utils.jl")
 
-include("for.jl")
-include("dist.jl")
-# include("flat.jl")
-include("iid.jl")
-include("utils.jl")
-include("dependencies.jl")
-include("logpdf.jl")
-include("weighted.jl")
-include("likelihood-weighting.jl")
-# include("examples.jl")
-# include("graph.jl")
-include("dynamicHMC.jl")
-# # include("optim.jl")
-include("importance.jl")
-include("canonical.jl")
-include("symbolic.jl")
-include("codegen.jl")
-# # include("sobols.jl")
-# # include("fromcube.jl")
-# # include("tocube.jl")
+include("distributions/dist.jl")
+include("distributions/for.jl")
+include("distributions/iid.jl")
+include("distributions/mix.jl")
+# include("distributions/flat.jl")
+
+include("primitives/rand.jl")
+include("primitives/logpdf.jl")
+include("primitives/xform.jl")
+include("primitives/likelihood-weighting.jl")
+
+
+include("transforms/predictive.jl")
+include("transforms/markovblanket.jl")
+
+include("symbolic/symbolic.jl")
+include("symbolic/codegen.jl")
+
 include("particles.jl")
-include("xform.jl")
-include("toposort.jl")
-include("advancedhmc.jl")
-# include("plots.jl")
-# include("rejection.jl")
-include("rand.jl")
-include("predictive.jl")
-include("mix.jl")
 include("plots.jl")
-include("markovblanket.jl")
+
+include("inference/rejection.jl")
+include("inference/dynamicHMC.jl")
+include("inference/advancedhmc.jl")
+
+
+# include("weighted.jl")
+# 
+# # include("graph.jl")
+# # # include("optim.jl")
+# include("importance.jl")
+# 
+# # # include("sobols.jl")
+# # # include("fromcube.jl")
+# # # include("tocube.jl")
 end # module
