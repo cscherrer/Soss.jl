f5bd51f20f07cc06cf7f29cc7610aec65a8f4140
Chad Scherrer <chad.scherrer@gmail.com>
2019-11-19 18:52:59 -0800

    delete for refactoring to other repos (bit scary)

--- src/exponentialFamilies.jl
+++
@@ -1,9 +0,0 @@
-using SimplTraits
-@traitdef IsExpFam{D}
-@traitimpl IsExpFam{D} <- isExpFam(D)
-
-isExpFam(D) = false
-
-@traitimpl IsExpFam{Normal{T} where T} = true
-
-@traitfn f(d::D) where {D; IsExpFam{D}} 
