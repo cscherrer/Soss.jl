f6317c4b7953f8de4492248b103e5a5bc4742fe4
Chad Scherrer <chad.scherrer@gmail.com>
2019-11-24 20:18:42 -0800

    add bodyVariables

--- src/core/utils.jl
+++ src/core/utils.jl
@@ -44,6 +44,7 @@ stochastic(m::Model) = keys(m.dists)
 export bound
 bound(m::Model) = keys(m.vals)
 
+bodyVariables(m::Model) = setdiff(variables(m), arguments(m))
 
 # TODO: Fix these broken methods
 # export observed
@@ -55,10 +56,7 @@ bound(m::Model) = keys(m.vals)
 #     observed(m)
 # )
 
-export freeVariables
-function freeVariables(m::Model)
-    setdiff(arguments(m), stochastic(m))
-end
+
 
 export foldall
 function foldall(leaf, branch; kwargs...) 
