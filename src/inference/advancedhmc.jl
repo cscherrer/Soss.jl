86214486b4aacae7c93c48d09ba7af94017f1a63
Vassily Trubetskoy <3219751+v-a-s-a@users.noreply.github.com>
2020-05-13 00:26:08 +0200

    Initial docstrings and doctests for inference methods (#142)
    * Initial doctest for `advancedHMC`
    
    Simple docstring and test for the `advancedHMC` function. Notes:
      - Raw prefix to docstring is necessary to correctly parse the output filter.
      - E[β|x] is not deterministic, so it currently only checks the first two significant digits.
    
    * finely control output generated by doctest
    
    Switched from attempting to filter generated out, to switching off sampling progress printting and deprecation warnings.
    
    * Change to flagging deprecation warnings
    
    Revert the change to the travis config, and update the `advancedHMC` sampling call based on their current docs.
    
    * initial docstring and doctest for `dynamicHMC`
    
    This follows the docstring and doctest for `advancedHMC`. The `method` argument is undocumented because I couldn't figure out exactly what it is doing.
    
    * Update src/inference/dynamicHMC.jl
    
    Co-authored-by: Chad Scherrer <chad.scherrer@gmail.com>
    
    Co-authored-by: Chad Scherrer <chad.scherrer@gmail.com>

--- src/inference/advancedhmc.jl
+++ src/inference/advancedhmc.jl
@@ -12,6 +12,51 @@ using AdvancedHMC
 export advancedHMC
 
 
+
+"""
+    advancedHMC(m, data, N = 1000; n_adapts = 1000)
+
+Draw `N` samples from the posterior distribution of parameters defined in Soss model `m`, conditional on `data`. Samples are drawn using Hamiltonial Monte Carlo (HMC) from the `advancedHMC.jl` package.
+
+## Keywords
+*  `n_adapts = 1000`: The number of interations used to set HMC parameters.
+
+Returns a tuple of length 2:
+1. Samples from the posterior distribution of parameters.  
+2. Sample summary statistics.  
+
+## Example
+
+```jldoctest
+
+using Random
+Random.seed!(42);
+
+m = @model x begin
+    β ~ Normal()
+    yhat = β .* x
+    y ~ For(eachindex(x)) do j
+        Normal(yhat[j], 2.0)
+    end
+end
+
+x = randn(3);
+truth = rand(m(x=x));
+
+post = advancedHMC(m(x=x), (y=truth.y,));
+E_β = mean(post[1])[1]
+
+println("true β: " * string(round(truth.β, digits=2)))
+println("Posterior mean β: " * string(round(E_β, digits=2)))
+
+# output
+true β: -0.3
+Posterior mean β: -0.25
+```
+
+
+
+"""
 function advancedHMC(m :: JointDistribution{A,B}, _data, N = 1000; 
                                                          n_adapts  = 1000) where {A,B}
     
@@ -36,18 +81,20 @@ function advancedHMC(m :: JointDistribution{A,B}, _data, N = 1000;
 
     D = t.dimension
     # Draw a random starting points
-    x_init = randn(D)
+    initial_θ = randn(D)
 
     # Define metric space, Hamiltonian, sampling method and adaptor
     metric = DiagEuclideanMetric(D)
-    h = Hamiltonian(metric, f, ∂f)
-    prop = AdvancedHMC.NUTS(Leapfrog(find_good_eps(h, x_init)))
-    adaptor = StanHMCAdaptor(n_adapts, Preconditioner(metric), NesterovDualAveraging(0.8, prop.integrator.ϵ))
+    hamiltonian = Hamiltonian(metric, f, ∂f)
+    prop = AdvancedHMC.NUTS(Leapfrog(find_good_stepsize(hamiltonian, initial_θ)))
+    initial_ϵ = find_good_stepsize(hamiltonian, initial_θ)
+    integrator = Leapfrog(initial_ϵ)
+    adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, integrator))
 
     # Draw samples via simulating Hamiltonian dynamics
     # - `samples` will store the samples
     # - `stats` will store statistics for each sample
-    samples, stats = sample(h, prop, x_init, N, adaptor, n_adapts; progress=true)
+    samples, stats = sample(hamiltonian, prop, initial_θ, N, adaptor, n_adapts; progress=false, verbose=false)
 
 end
 
--- src/inference/dynamicHMC.jl
+++ src/inference/dynamicHMC.jl
@@ -9,6 +9,69 @@ import LogDensityProblems: ADgradient
 
 export dynamicHMC
 
+"""
+    dynamicHMC(
+        rng::AbstractRNG,
+        m::JointDistribution,
+        _data,
+        N::Int = 1000;
+        method = logpdf,
+        ad_backend = Val(:ForwardDiff),
+        reporter = DynamicHMC.NoProgressReport(),
+        kwargs...)
+    
+
+Draw `N` samples from the posterior distribution of parameters defined in Soss model `m`, conditional on `_data`. Samples are drawn using Hamiltonial Monte Carlo (HMC) from the `DynamicHMC.jl` package.
+
+This function is essentially a wrapper around `DynamicHMC.mcmc_with_warmup()`. Arguments `reporter`, `ad_backend` [DynamicHMC docs here](https://tamaspapp.eu/DynamicHMC.jl/stable/interface/#DynamicHMC.mcmc_with_warmup))
+
+## Arguments
+* `rng`: Random number generator.
+* `m`: Soss model.
+* `_data`: `NamedTuple` of data to condition on.
+
+
+## Keyword Arguments
+*  `N = 1000`: Number of samples to draw.
+*  `method = logpdf`: How to compute the log-density. Options are `logpdf` (delegates to `logpdf` of each component) or `codegen` (symbolic simplification and code generation).
+*  `ad_backend = Val(:ForwardDiff)`: Automatic differentiation backend.
+*  `reporter = DynamicHMC.NoProgressReport()`: Specify logging during sampling. Default: do not log progress.
+*  `kwargs`: Additional keyword arguments passed to core sampling function `DynamicHMC.mcmc_with_warmup()`.
+
+
+Returns an Array of `Namedtuple` of length `N`. Each entry in the array is a sample of parameters indexed by the parameter symbol.
+
+## Example
+
+```jldoctest
+
+using Random
+Random.seed!(42);
+rng = MersenneTwister(42);
+
+m = @model x begin
+    β ~ Normal()
+    yhat = β .* x
+    y ~ For(eachindex(x)) do j
+        Normal(yhat[j], 2.0)
+    end
+end
+
+x = randn(50);
+truth = rand(m(x=x));
+
+post = dynamicHMC(rng, m(x=x), (y=truth.y,));
+E_β = mean(getfield.(post, :β))
+
+println("true β: " * string(round(truth.β, digits=2)))
+println("Posterior mean β: " * string(round(E_β, digits=2)))
+
+# output
+true β: 0.3
+Posterior mean β: 0.47
+```
+
+"""
 function dynamicHMC(
     rng::AbstractRNG,
     m::JointDistribution,
