cd72544eda17979e5db8c9bacdffb8a35d80b7a3
Joey Miller <arch1190@gmail.com>
2020-08-31 18:16:28 -0500

    `weave()` no longer takes "throw_errors" kwarg. It's a chunk-option now.

--- test/readme.jl
+++ test/readme.jl
@@ -2,7 +2,8 @@ using Weave
 using Documenter
 
 function buildREADME()
-    weave("../README.jmd", doctype= "github", throw_errors=true, cache=:refresh, args=Dict(:seed => 6))
+    set_chunk_defaults!(:error=>false)
+    weave("../README.jmd", doctype= "github", cache=:refresh, args=Dict(:seed => 6))
     return true
 end
 
