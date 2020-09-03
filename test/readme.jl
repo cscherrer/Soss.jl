570ffc2e97c0fa2127cf390a8f48098e05cef580
Joseph <jx1525@att.com>
2020-09-03 10:58:35 -0500

    change tests to approx equality. Seeing differences on different hardware

--- test/readme.jl
+++ test/readme.jl
@@ -33,6 +33,6 @@ jointdist = m2(X=X)
 
 @testset "logpdf, symlogpdf" begin
     @test_nowarn symlogpdf(m2).evalf(3)
-    @test logpdf(jointdist, truth)==-28.551921801470908
-    @test logpdf(jointdist, truth, codegen)==-28.551921801470904
+    @test logpdf(jointdist, truth) ≈ -28.551921801470908
+    @test logpdf(jointdist, truth, codegen) ≈ -28.551921801470904
 end
