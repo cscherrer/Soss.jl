a3ecb541d00cbad55fde6311742b56218fc4c820
Chad Scherrer <chad.scherrer@gmail.com>
2021-04-11 08:33:41 -0700

    interpret

---
+++ src/primitives/interpret.jl
@@ -0,0 +1,27 @@
+export interpret
+
+function interpret(m::ASTModel{A,B,M}) where {A,B,M}
+    theModule = from_type(M)
+    mk_function(theModule, _interpret(m.body))
+end
+
+function _interpret(ast::Expr)
+    function branch(head, newargs)
+        expr = Expr(head, newargs...)
+        (head, newargs[1]) == (:call, :~) || return expr
+        length(newargs) == 3 || return expr
+
+        (_, x, d) = newargs
+        :(($x, _ctx) = _tilde($(QuoteNode(x)), $d, _ctx))
+    end
+
+    body = foldall(identity, branch)(ast)
+
+    :(
+        (_tilde, _ctx0) -> begin
+            _ctx = _ctx0
+            $(body.args...)
+            return _ctx
+        end
+    ) 
+end
