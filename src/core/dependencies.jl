c2a850916a4d38f492934751ad78340b2a94fb74
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-01 12:36:38 -0700

    Drop redundant code

--- src/core/dependencies.jl
+++ src/core/dependencies.jl
@@ -2,32 +2,6 @@ using JuliaVariables
 using MLStyle
 import MacroTools
 
-
-
-unwrap_scoped(ex) = @match ex begin
-    Expr(:scoped, _, a) => unwrap_scoped(a)
-    Expr(head, args...) => Expr(head, map(unwrap_scoped, args)...)
-    a => a
-end
-
-globals(s::Symbol) = [s]
-
-function globals(ex::Expr)
-    branch(head, newargs) = union(newargs...)
-
-    function leaf(v::JuliaVariables.Var)
-        v.is_global ? [v.name] : Symbol[]
-    end
-
-    leaf(x) = []
-
-    solved_ex = unwrap_scoped(solve_from_local!(simplify_ex(ex)))
-
-    return foldall(leaf, branch)(solved_ex)
-end
-
-
-
 unwrap_scoped(ex) = @match ex begin
     Expr(:scoped, _, a) => unwrap_scoped(a)
     Expr(head, args...) => Expr(head, map(unwrap_scoped, args)...)
