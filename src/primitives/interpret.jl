2d860ee43dcdfedd3438e19bbeb6bcf2fc1abaf9
Chad Scherrer <chad.scherrer@gmail.com>
2021-07-15 13:53:48 -0700

    trying out `rand`

--- src/primitives/interpret.jl
+++ src/primitives/interpret.jl
@@ -1,11 +1,11 @@
 export interpret
 
-function interpret(m::ASTModel{A,B,M}, _tilde; call=nothing) where {A,B,M}
+function interpret(m::ASTModel{A,B,M}, _tilde, call=nothing) where {A,B,M}
     theModule = from_type(M)
     mk_function(theModule, _interpret(m.body, _tilde; call=call))
 end
 
-function _interpret(ast::Expr, _tilde; call=nothing)
+function _interpret(ast::Expr, _tilde, _ctx0, call=nothing)
     function branch(head, newargs)
         expr = Expr(head, newargs...)
         (head, newargs[1]) == (:call, :~) || return expr
@@ -18,14 +18,43 @@ function _interpret(ast::Expr, _tilde; call=nothing)
     body = foldall(identity, branch)(ast)
 
     if !isnothing(call)
-        body = Soss.callify(body; call=call)
+        body = callify(body; call=call)
     end
 
-    :(
-        _ctx0 -> begin
-            _ctx = _ctx0
-            $(body.args...)
-            return _ctx
-        end
-    )
+    quote
+        _ctx = $_ctx0
+        $(body.args...)
+        _ctx
+    end
 end
+
+@inline function Base.rand(rng::AbstractRNG, m::ASTModel; call=nothing)
+    return _rand(getmoduletypencoding(m), m, NamedTuple(), call)(rng)
+end
+
+@gg function _rand(M::Type{<:TypeLevel}, _m::ASTModel, _args, call)
+    call = call.instance
+    function tilde(v::Val, d, ctx)
+        x = rand(d)
+        ctx = merge(ctx, NamedTuple{(unVal(v),)}((x,)))
+        (x, ctx)
+    end
+
+    ctx0 = NamedTuple()
+
+    body = type2model(_m).body |> loadvals(_args, NamedTuple())
+    body = _interpret(body, tilde, ctx0, call)
+
+    @under_global from_type(_unwrap_type(M)) @q let M
+        _rng -> begin
+            $body
+        end
+    end
+end
+
+# @gg function _rand(M::Type{<:TypeLevel}, _m::ASTModel, _args::NamedTuple{()})
+#     body = type2model(_m) |> sourceRand()
+#     @under_global from_type(_unwrap_type(M)) @q let M
+#         $body
+#     end
+# end
