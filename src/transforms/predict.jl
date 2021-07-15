3fc0d18f98dc087182196999b92d87a017fc5cb5
Chad Scherrer <chad.scherrer@gmail.com>
2021-07-15 15:55:06 -0700

    Better `predict` method

--- src/transforms/predict.jl
+++ src/transforms/predict.jl
@@ -1,4 +1,5 @@
 export predict
+using TupleVectors
 
 function predict(d::ConditionalModel, post::Vector{NamedTuple{N,T}}) where {N,T}
     args = argvals(d)
@@ -31,6 +32,24 @@ predict(m::Model; kwargs...) = predict(m,(;kwargs...))
 
 predict(d,x) = x
 
-function predict(d, s::AbstractVector)
-    [predict(d, sj) for sj in s]
+predict(d::ConditionalModel, post) = predict(Random.GLOBAL_RNG, d, post)
+
+function predict(rng::AbstractRNG, d::ConditionalModel, post::AbstractVector{<:NamedTuple{N}}) where {N}
+    m = Model(d)
+    pred = predictive(m, N...)
+    args = argvals(d)
+    y1 = rand(rng, pred(merge(args,post[1])))
+    n = length(post)
+    v = TupleVectors.chainvec(y1, n)
+    for j in 2:n
+        v[j] = rand(rng, pred(merge(args,post[j])))
+    end
+
+    v
+end
+
+using SampleChains
+
+function predict(rng::AbstractRNG, d::ConditionalModel, post::MultiChain)
+    [predict(rng, d, c) for c in getchains(post)]
 end
