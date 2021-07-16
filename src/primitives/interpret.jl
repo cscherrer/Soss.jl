b1b213bce598f8043c42a8b9be3a3feb37b27dcb
Chad Scherrer <chad.scherrer@gmail.com>
2021-07-16 09:57:00 -0700

    make it faster

--- src/primitives/interpret.jl
+++ src/primitives/interpret.jl
@@ -28,16 +28,16 @@ function _interpret(ast::Expr, _tilde, _ctx0, call=nothing)
     end
 end
 
-function mkfun(f, M, _m, _args, _obs, tilde, ctx0, call)
+function mkfun(f, _m, _args, _obs, tilde, ctx0, call)
     call = call.instance
     _m = type2model(_m)
-
+    M = getmodule(_m)
 
     body = _m.body |> loadvals(_args, NamedTuple())
     body = _interpret(body, tilde, ctx0, call)
 
     body = f(body)
-    @under_global from_type(_unwrap_type(M)) @q let M
+    @under_global M @q let M
         $body
     end
 end
@@ -45,10 +45,11 @@ end
 
 
 @inline function Base.rand(rng::AbstractRNG, m::ASTModel; call=nothing)
-    return _rand(getmoduletypencoding(m), m, NamedTuple(), call)(rng)
+    return _rand(m, NamedTuple(), call)(rng)
 end
 
-@gg function _rand(M::Type{<:TypeLevel}, _m::ASTModel, _args, call)
+@gg function _rand(_m::ASTModel, _args, call)
+    
     _obs = NamedTuple()
     function tilde(v::Val, d, ctx)
         x = rand(d)
@@ -64,12 +65,5 @@ end
         end
     end
 
-    mkfun(f, M, _m, _args, _obs, tilde, ctx0, call)
+    mkfun(f, _m, _args, _obs, tilde, ctx0, call)
 end
-
-# @gg function _rand(M::Type{<:TypeLevel}, _m::ASTModel, _args::NamedTuple{()})
-#     body = type2model(_m) |> sourceRand()
-#     @under_global from_type(_unwrap_type(M)) @q let M
-#         $body
-#     end
-# end
