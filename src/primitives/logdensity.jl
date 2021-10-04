839defd06ca0f6f1ae066832ee088303e2b40b52
Chad Scherrer <chad.scherrer@gmail.com>
2021-10-04 13:16:45 -0700

    add logpdf method for models

--- src/primitives/logdensity.jl
+++ src/primitives/logdensity.jl
@@ -46,3 +46,47 @@ end
         $body
     end
 end
+
+
+function MeasureTheory.logpdf(c::ConditionalModel{A,B,M}, x=NamedTuple()) where {A,B,M}
+    _logpdf(M, Model(c), argvals(c), observations(c), x)
+end
+
+export sourceLogpdf
+
+sourceLogpdf(m::AbstractModel) = sourceLogpdf()(Model(m))
+
+function sourceLogpdf()
+    function(_m::Model)
+        proc(_m, st :: Assign)     = :($(st.x) = $(st.rhs))
+        # proc(_m, st :: Sample)     = :(_ℓ += logpdf($(st.rhs), $(st.x)))
+        proc(_m, st :: Return)     = nothing
+        proc(_m, st :: LineNumber) = nothing
+        function proc(_m, st :: Sample)
+            x = st.x
+            rhs = st.rhs
+            @q begin
+                _ℓ += Soss.logpdf($rhs, $x)
+                $x = Soss.predict($rhs, $x)
+            end
+        end
+
+        wrap(kernel) = @q begin
+            _ℓ = 0.0
+            $kernel
+            return _ℓ
+        end
+
+        buildSource(_m, proc, wrap) |> MacroTools.flatten
+    end
+end
+
+# MeasureTheory.logpdf(d::Distribution, val, tr) = logpdf(d, val)
+
+
+@gg function _logpdf(M::Type{<:TypeLevel}, _m::Model, _args, _data, _pars)
+    body = type2model(_m) |> sourceLogpdf() |> loadvals(_args, _data, _pars)
+    @under_global from_type(_unwrap_type(M)) @q let M
+        $body
+    end
+end
