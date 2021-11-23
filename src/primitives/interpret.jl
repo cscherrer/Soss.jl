34be83980fa6321abfd715bd6f2631d410f00c5c
Chad Scherrer <chad.scherrer@gmail.com>
2021-11-23 07:58:29 -0800

    gotos

--- src/primitives/interpret.jl
+++ src/primitives/interpret.jl
@@ -5,25 +5,41 @@ function interpret(m::ASTModel{A,B,M}, tilde, ctx0) where {A,B,M}
     mk_function(theModule, _interpret(m.body, tilde, ctx0))
 end
 
-function _interpret(ast::Expr, _tilde, _args, _obs)
-    function go(ex)
+function _interpret(ast::Expr, _tilde, pars, _args, _obs)
+    function gotos(pars)
+        q = quote end
+        for par in pars
+            qpar = QuoteNode(par)
+            par_label = QuoteNode(Symbol(Sample_, $qpar))
+            push!(q.args, 
+            quote
+                _next_command = _driver()
+                $(Expr(:symbolicgoto, $par_label))
+
+            end
+        end
+        return q
+    end
+
+    function new_ast(ex)
         @match ex begin
             :($(x::Symbol) ~ $d) => begin
                 qx = QuoteNode(x)
                 inargs = Val(x ∈ getntkeys(_args))
                 inobs = Val(x ∈ getntkeys(_obs))
                 quote
+                    :($(Expr(:symboliclabel, Symbol(Sample_, $qx))))
                     ($x, _ctx, _retn) = $_tilde($qx, $d, _cfg, _ctx, $inargs, $inobs)
                 end
             end
 
-            Expr(head, args...) => Expr(head, map(go, args)...)
+            Expr(head, args...) => Expr(head, map(new_ast, args)...)
             
             x => x
         end
     end
 
-    body = go(ast)
+    body = new_ast(ast)
 
     body
 end
@@ -37,9 +53,11 @@ end
 
     tilde = T.instance
     call = C.instance
-     
+    
+    pars = parameters(_m)
+
     body = _m.body |> loadvals(_args, _obs)
-    body = _interpret(body, tilde, _args, _obs)
+    body = _interpret(body, tilde, pars, _args, _obs)
 
     q = (@q let M
         @inline function(_cfg, _ctx)
