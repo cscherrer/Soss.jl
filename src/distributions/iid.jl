e7bd0d3ea36a7bf579d2a80b2c925320e50a6892
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-29 11:10:24 -0800

    have `iid` use powermeasure

--- src/distributions/iid.jl
+++ src/distributions/iid.jl
@@ -5,4 +5,4 @@ iid(n::Int...) = dist -> iid(dist, n...)
 
 iid(dist::AbstractMeasure, n...) = dist ^ n
 
-iid(dist::Dists.Distribution, n) = Dists.Product(fill(dist, n))
+iid(dist::Dists.Distribution, n...) = MeasureBase.powermeasure(dist, n)
