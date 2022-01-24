3b1f36f75c9b6f45ad6ac8e065eb66cfdf729d11
Chad Scherrer <chad.scherrer@gmail.com>
2022-01-24 15:28:59 -0800

    clean up insupport

--- src/primitives/insupport.jl
+++ src/primitives/insupport.jl
@@ -18,7 +18,6 @@ sourceInsupport(m::AbstractModel) = sourceInsupport()(Model(m))
 function sourceInsupport()
     function(_m::Model)
         proc(_m, st :: Assign)     = :($(st.x) = $(st.rhs))
-        # proc(_m, st :: Sample)     = :(_ℓ += insupport($(st.rhs), $(st.x)))
         proc(_m, st :: Return)     = nothing
         proc(_m, st :: LineNumber) = nothing
         function proc(_m, st :: Sample)
@@ -48,46 +47,3 @@ end
         $body
     end
 end
-
-
-# function logdensityof(c::ConditionalModel{A,B,M}, x=NamedTuple()) where {A,B,M}
-#     _logdensityof(M, Model(c), argvals(c), observations(c), x)
-# end
-
-
-# sourceLogdensityof(m::AbstractModel) = sourceLogdensityof()(Model(m))
-
-# function sourceLogdensityof()
-#     function(_m::Model)
-#         proc(_m, st :: Assign)     = :($(st.x) = $(st.rhs))
-#         # proc(_m, st :: Sample)     = :(_ℓ += logdensityof($(st.rhs), $(st.x)))
-#         proc(_m, st :: Return)     = nothing
-#         proc(_m, st :: LineNumber) = nothing
-#         function proc(_m, st :: Sample)
-#             x = st.x
-#             rhs = st.rhs
-#             @q begin
-#                 _ℓ += Soss.logdensityof($rhs, $x)
-#                 $x = Soss.predict($rhs, $x)
-#             end
-#         end
-
-#         wrap(kernel) = @q begin
-#             _ℓ = 0.0
-#             $kernel
-#             return _ℓ
-#         end
-
-#         buildSource(_m, proc, wrap) |> MacroTools.flatten
-#     end
-# end
-
-# # MeasureTheory.logdensityof(d::Distribution, val, tr) = logdensityof(d, val)
-
-
-# @gg function _logdensityof(M::Type{<:TypeLevel}, _m::Model, _args, _data, _pars)
-#     body = type2model(_m) |> sourceLogdensityof() |> loadvals(_args, _data, _pars)
-#     @under_global from_type(_unwrap_type(M)) @q let M
-#         $body
-#     end
-# end
