4dc0bba59478032813f977cf17f376b0ee9f3542
Taine Zhao <twshere@outlook.com>
2022-02-15 23:00:09 +0900

    fix-JET-infer (#321)
    * fix-JET-infer
    
    * fix

--- src/primitives/interpret.jl
+++ src/primitives/interpret.jl
@@ -53,7 +53,7 @@ end
     _obs = obstype(MC)
 
     tilde = T.instance
-     
+
     body = _m.body |> loadvals(_args, _obs)
     body = _interpret(M, body, tilde, _args, _obs)
 
@@ -69,4 +69,36 @@ end
     end)
 
     @under_global M q
+end
+
+
+function _get_gg_func_body(::RuntimeFn{Args,Kwargs,Body}) where {Args,Kwargs,Body}
+    Body
+end
+
+function _get_gg_func_body(ex)
+    error(ex)
+end
+
+@generated function mkfun_call(_mc::MC, ::T, _cfg, _ctx) where {MC, T}
+    _m = type2model(MC)
+    M = getmodule(_m)
+
+    _args = argvalstype(MC)
+    _obs = obstype(MC)
+
+    tilde = T.instance
+    body = _m.body |> loadvals(_args, _obs)
+    body = _interpret(M, body, tilde, _args, _obs)
+
+    q = MacroTools.flatten(@q function (_mc, _cfg, _ctx)
+            local _retn
+            _args = Soss.argvals(_mc)
+            _obs = Soss.observations(_mc)
+            _cfg = merge(_cfg, (args=_args, obs=_obs))
+            $body
+            _retn
+        end)
+
+    from_type(_get_gg_func_body(mk_function(M, q)))
 end
